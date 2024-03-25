838911676cb49179f562b38d74bdb9d9fa3a27e0
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-03-25 14:57:51 +0100

    feat(api): added dto class for chapter

---
+++ api/src/main/java/de/storyteller/api/dto/ChapterDTO.java
@@ -0,0 +1,20 @@
+package de.storyteller.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChapterDTO {
+    private Long id;
+    private Long bookId;
+    private String chapterTitle;
+    private String content;
+    private LocalDateTime lastModified;
+}
