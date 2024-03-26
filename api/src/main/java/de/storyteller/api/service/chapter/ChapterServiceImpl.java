7b9e1afa6fe631aecb8746da4cb0cce16f1d9a35
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-03-26 19:09:06 +0100

    fix(api): added exceptions

--- api/src/main/java/de/storyteller/api/service/chapter/ChapterServiceImpl.java
+++ api/src/main/java/de/storyteller/api/service/chapter/ChapterServiceImpl.java
@@ -6,6 +6,7 @@ import de.storyteller.api.dto.chapter.ChapterDTO;
 import de.storyteller.api.dto.chapter.EditChapterRequest;
 import de.storyteller.api.mapper.ChapterMapper;
 import de.storyteller.api.model.Chapter;
+import de.storyteller.api.repository.BookRepository;
 import de.storyteller.api.repository.ChapterRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,14 +18,24 @@ import java.util.stream.Collectors;
 @Service
 public class ChapterServiceImpl implements ChapterService{
     private final ChapterRepository chapterRepository;
+    private final BookRepository bookRepository;
     private final ChapterMapper chapterMapper;
     @Override
     public ChapterDTO createChapter(AddChapterRequest chapter) {
+        if (!bookRepository.existsById(chapter.getBookId())) {
+            throw new RuntimeException("Book with id: " + chapter.getBookId() + " doesn't exist");
+        }
         return chapterMapper.toChapterDTO(chapterRepository.save(chapterMapper.toChapter(chapter)));
     }
 
     @Override
     public ChapterDTO updateChapter(EditChapterRequest chapter) {
+        if (!chapterRepository.existsById(chapter.getId())) {
+            throw new RuntimeException("Chapter with id: " + chapter.getId() + " doesn't exist");
+        }
+        if (!bookRepository.existsById(chapter.getBookId())) {
+            throw new RuntimeException("Book with id: " + chapter.getBookId() + " doesn't exist");
+        }
         return chapterMapper.toChapterDTO(chapterRepository.save(chapterMapper.toChapter(chapter)));
     }
 
