0205823ce9fec91b86cbfa1c426a741d9fa8da60
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-04-02 16:12:16 +0200

    feat(webapp): added routes

--- webapp/storyteller/src/app/app-routing.module.ts
+++ webapp/storyteller/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DiscoveryPageComponent} from "./components/discovery-page/discovery-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/discovery', pathMatch: 'full' },
+  { path: 'discovery', component: DiscoveryPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
