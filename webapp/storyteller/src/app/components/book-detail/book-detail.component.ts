d8b730a5e6821f27a8f8e263fd1d9fd069807f1d
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-04-03 14:49:33 +0200

    feat(webapp): added methods to get a book

--- webapp/storyteller/src/app/components/book-detail/book-detail.component.ts
+++ webapp/storyteller/src/app/components/book-detail/book-detail.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {BookService} from "../../service/book.service";
+import {Book} from "../../model/book";
 
 @Component({
   selector: 'app-book-detail',
   templateUrl: './book-detail.component.html',
   styleUrl: './book-detail.component.scss'
 })
-export class BookDetailComponent {
+export class BookDetailComponent implements OnInit{
+  private bookId!: string;
+  private book!: Book;
+
+  constructor(private route: ActivatedRoute, private bookService: BookService) {}
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.bookId = params['bookId'];
+    });
+  }
+
+  getBookById(): void {
+    this.bookService.getBookById(this.bookId).subscribe({
+      next: (resp: Book) => {
+        this.book = resp;
+      },
+      error: (error: any) => {
+        console.error(error.message);
+      }
+    });
+  }
+
+
+
 
 }
