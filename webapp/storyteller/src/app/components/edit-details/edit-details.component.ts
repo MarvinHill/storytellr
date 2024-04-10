d50070edc8ab2091395765bfb3a257487fe6a160
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-04-10 16:03:24 +0200

    feat(webapp): added layout from book detail page

--- webapp/storyteller/src/app/components/edit-details/edit-details.component.ts
+++ webapp/storyteller/src/app/components/edit-details/edit-details.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {KeycloakService} from "keycloak-angular";
+import {BookService} from "../../service/book.service";
+import {UserServiceService} from "../../service/user-service.service";
+import {Book} from "../../model/book";
 
 @Component({
   selector: 'app-edit-details',
@@ -7,12 +10,16 @@ import {KeycloakService} from "keycloak-angular";
   styleUrl: './edit-details.component.scss'
 })
 export class EditDetailsComponent implements OnInit{
+  book?: Book;
 
-  constructor(private keyCloakService: KeycloakService) {
+  constructor(private keyCloakService: KeycloakService, private bookService: BookService, private userService: UserServiceService) {
   }
 
   ngOnInit() {
-    console.log(this.keyCloakService.getUsername());
+    this.keyCloakService.getToken()
+      .then(token => {
+        console.log(this.userService.getUserIdFromJWT(token));
+      });
   }
 
 }
