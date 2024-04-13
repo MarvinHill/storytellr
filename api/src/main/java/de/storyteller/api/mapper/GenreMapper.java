c1fc1d9c8de73c90ead195ef3bd58f4bfd2fbee4
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-04-13 10:46:33 +0200

    fix(api): handled warnings

--- api/src/main/java/de/storyteller/api/mapper/GenreMapper.java
+++ api/src/main/java/de/storyteller/api/mapper/GenreMapper.java
@@ -1,20 +1,20 @@
 package de.storyteller.api.mapper;
 
-import de.storyteller.api.dto.chapter.ChapterDTO;
 import de.storyteller.api.dto.genre.AddGenreRequest;
 import de.storyteller.api.dto.genre.GenreDTO;
 import de.storyteller.api.model.Genre;
 import de.storyteller.api.repository.GenreRepository;
+import lombok.RequiredArgsConstructor;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 @Mapper(componentModel = "spring")
 public abstract class GenreMapper {
-    @Autowired
-    protected GenreRepository genreRepository;
+
+    private final GenreRepository genreRepository;
 
     public abstract Genre toGenre(GenreDTO genreDTO);
     public abstract GenreDTO toGenreDTO(Genre genre);
