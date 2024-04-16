10de01aec4e8713f6e555600b9d198844e5b7860
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-04-16 18:27:02 +0200

    feat(webapp): made progress in updating the books

--- webapp/storyteller/src/app/components/edit-details/edit-details.component.ts
+++ webapp/storyteller/src/app/components/edit-details/edit-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {KeycloakService} from "keycloak-angular";
 import {BookService} from "../../service/book.service";
 import {UserService} from "../../service/user.service";
@@ -6,6 +6,8 @@ import {Book} from "../../model/book";
 import {ActivatedRoute} from "@angular/router";
 import {BookMapperService} from "../../service/book-mapper.service";
 import {FormBuilderService} from "../../service/form-builder.service";
+import {GenreService} from "../../service/genre.service";
+import {Genre} from "../../model/genre";
 
 @Component({
   selector: 'app-edit-details',
@@ -17,20 +19,25 @@ export class EditDetailsComponent implements OnInit{
   bookId!: string;
   titleEdit = false;
   descriptionEdit = false;
+  catchphraseEdit= false;
+  genreEdit = false;
+  genres: Genre[] = [];
 
   constructor(private keyCloakService: KeycloakService, private bookService: BookService, private userService: UserService, private route: ActivatedRoute,
-              private bookMapperService: BookMapperService) {
+              private bookMapperService: BookMapperService, private genreService: GenreService) {
   }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.route.queryParams.subscribe(params => {
       this.bookId = params['bookId'];
     });
-    this.getBookById();
+    this.getBookById(this.bookId);
+    this.getGenres();
   }
 
-  getBookById(): void {
-    this.bookService.getBookById(this.bookId).subscribe({
+
+  getBookById(bookId: string) {
+    this.bookService.getBookById(bookId).subscribe({
       next: (resp: Book) => {
         this.book = resp;
       },
@@ -38,7 +45,9 @@ export class EditDetailsComponent implements OnInit{
         console.error(error.message);
       }
     });
+
   }
+
 
   updateTitle() {
     this.titleEdit = !this.titleEdit;
@@ -55,6 +64,43 @@ export class EditDetailsComponent implements OnInit{
 
   updateDescription() {
     this.descriptionEdit = !this.descriptionEdit;
+    let editBook = this.bookMapperService.toEditBookRequest(this.book);
+    this.bookService.updateBook(editBook).subscribe({
+      next: (resp) => {
+        console.log("Response:" + resp);
+      },
+      error: (error) => {
+        console.error(error.message);
+      }
+    });
+  }
+
+  updateCatchphrase() {
+    this.catchphraseEdit = !this.catchphraseEdit;
+    let editBook = this.bookMapperService.toEditBookRequest(this.book);
+    this.bookService.updateBook(editBook).subscribe({
+      next: (resp) => {
+      },
+      error: (error) => {
+        console.error(error.message);
+      }
+    });
+  }
+
+  getGenres() {
+    this.genreService.getAllGenres().subscribe({
+      next: (resp: Genre[]) => {
+        console.log(resp);
+        this.genres = resp;
+      },
+      error: (error: any) => {
+        console.error(error.message);
+      }
+    });
+  }
+
+  updateGenre() {
+    this.genreEdit = !this.genreEdit;
     let editBook = this.bookMapperService.toEditBookRequest(this.book);
     this.bookService.updateBook(editBook).subscribe({
       next: (resp) => {
