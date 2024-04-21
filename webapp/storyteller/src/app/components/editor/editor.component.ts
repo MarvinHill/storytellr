3e0239c995a17525bf840d5bdcbbc50da6b2f9cf
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-04-21 16:37:58 +0200

    feat(webapp): added editor

--- webapp/storyteller/src/app/components/editor/editor.component.ts
+++ webapp/storyteller/src/app/components/editor/editor.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import EditorJS from "@editorjs/editorjs";
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.scss'
 })
-export class EditorComponent {
+export class EditorComponent implements OnInit{
+  editor: any;
+
+  ngOnInit() {
+    this.editor = new EditorJS({
+      holder: 'editorjs',
+    });
+  }
 
 }
