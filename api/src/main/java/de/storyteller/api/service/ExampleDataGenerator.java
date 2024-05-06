4010d9e2dca93869b1e8f643eda0545748e3e560
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-05-06 19:06:41 +0200

    feat(api): added generated genres

--- api/src/main/java/de/storyteller/api/service/ExampleDataGenerator.java
+++ api/src/main/java/de/storyteller/api/service/ExampleDataGenerator.java
@@ -1,5 +1,6 @@
 package de.storyteller.api.service;
 
+import de.storyteller.api.service.genre.GenreService;
 import de.storyteller.api.v1.dto.cover.CoverUriDTO;
 import de.storyteller.api.model.Book;
 import de.storyteller.api.model.Genre;
@@ -8,6 +9,8 @@ import de.storyteller.api.repository.GenreRepository;
 
 import java.util.List;
 import java.util.UUID;
+
+import de.storyteller.api.v1.dto.genre.AddGenreRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +20,9 @@ public class ExampleDataGenerator {
   GenreRepository genreRepository;
   @Autowired
   BookRepository bookRepository;
+
+  @Autowired
+  GenreService genreService;
 
   public void generate(){
     genreRepository.save(new Genre("1b8b0883-596d-42b0-be60-b8793f22a572","Fantasy"));
@@ -34,5 +40,26 @@ public class ExampleDataGenerator {
             true, false, false, false
       ));
     }
+  }
+
+  public void generateGenres(){
+    genreService.createGenre(new AddGenreRequest("Fantasy"));
+    genreService.createGenre(new AddGenreRequest("Distopian"));
+    genreService.createGenre(new AddGenreRequest("Mystery"));
+    genreService.createGenre(new AddGenreRequest("Science Fiction"));
+    genreService.createGenre(new AddGenreRequest("Romance"));
+    genreService.createGenre(new AddGenreRequest("Horror"));
+    genreService.createGenre(new AddGenreRequest("Thriller"));
+    genreService.createGenre(new AddGenreRequest("Historical Fiction"));
+    genreService.createGenre(new AddGenreRequest("Non-Fiction"));
+    genreService.createGenre(new AddGenreRequest("Biography"));
+    genreService.createGenre(new AddGenreRequest("Contemporary"));
+    genreService.createGenre(new AddGenreRequest("Young Adult"));
+    genreService.createGenre(new AddGenreRequest("New Adult"));
+    genreService.createGenre(new AddGenreRequest("Essays"));
+    genreService.createGenre(new AddGenreRequest("Poetry"));
+    genreService.createGenre(new AddGenreRequest("Short Stories"));
+    genreService.createGenre(new AddGenreRequest("Fan Fiction"));
+
   }
 }
