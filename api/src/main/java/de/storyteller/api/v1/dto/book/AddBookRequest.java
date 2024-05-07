a0d1e19d9a5413ab4498e7f67f03899754e92f4c
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-05-07 14:31:26 +0200

    feat(api): added validation for length

--- api/src/main/java/de/storyteller/api/v1/dto/book/AddBookRequest.java
+++ api/src/main/java/de/storyteller/api/v1/dto/book/AddBookRequest.java
@@ -2,6 +2,7 @@ package de.storyteller.api.v1.dto.book;
 
 import de.storyteller.api.model.Chapter;
 import de.storyteller.api.model.Genre;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -21,12 +22,15 @@ import java.util.UUID;
 @NoArgsConstructor
 public class AddBookRequest {
     @NotBlank(message = "Title should not be blank")
+    @Max(value = 100, message = "Title should not be longer than 100 characters")
     private String title;
 
     @NotNull(message = "A genre must be set")
     private String genreId;
     @NotBlank(message = "Description should not be blank")
+    @Max(value = 500, message = "Description should not be longer than 500 characters")
     private String description;
+    @Max(value = 200, message = "Catchphrase should not be longer than 200 characters")
     private String catchphrase;
     private List<String> chapterIds;
     private List<String> tags;
