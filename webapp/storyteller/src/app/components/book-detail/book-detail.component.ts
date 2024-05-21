a48c63481c9258f8ffca4ab37a5d45cc190b4998
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-05-21 16:07:32 +0200

    feat(webapp): added logic for adding a  removing book from library

--- webapp/storyteller/src/app/components/book-detail/book-detail.component.ts
+++ webapp/storyteller/src/app/components/book-detail/book-detail.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {BookService} from "../../service/book.service";
 import {Book} from "../../model/book";
+import {LibraryService} from "../../service/library.service";
 
 @Component({
   selector: 'app-book-detail',
@@ -11,14 +12,16 @@ import {Book} from "../../model/book";
 export class BookDetailComponent implements OnInit{
   bookId!: string;
   book?: Book;
+  bookInLibrary: boolean = false;
 
-  constructor(private route: ActivatedRoute, private bookService: BookService, private router: Router) {}
+  constructor(private route: ActivatedRoute, private bookService: BookService, private router: Router, private libraryService: LibraryService) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       this.bookId = params['bookId'];
     });
     this.getBookById();
+    this.isBookInLibrary();
   }
 
   getBookById(): void {
@@ -38,14 +41,38 @@ export class BookDetailComponent implements OnInit{
   }
 
   addToLibrary() {
-
+    this.libraryService.addBookToLibrary(this.bookId).subscribe({
+      next: (resp: Book) => {
+        console.log("Book added" + resp);
+        this.isBookInLibrary();
+      },
+      error: (error: any) => {
+        console.error(error.message);
+      }
+    });
   }
 
-  isBookInLibrary() {
-
-  }
+  isBookInLibrary(){
+    this.libraryService.isBookInLibrary(this.bookId).subscribe({
+      next: (resp: boolean) => {
+        console.log("Book in library: " + resp);
+        this.bookInLibrary = resp;
+      },
+      error: (error: any) => {
+        console.error(error.message);
+      }
+    });
+    }
 
   removeFromLibrary() {
-
+    this.libraryService.removeBookFromLibrary(this.bookId).subscribe({
+      next: (resp: Book) => {
+        console.log(resp);
+        this.isBookInLibrary();
+      },
+      error: (error: any) => {
+        console.error(error.message);
+      }
+    });
   }
 }
