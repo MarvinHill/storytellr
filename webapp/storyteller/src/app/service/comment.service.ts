81292e46b77c9022016afcce84a1607a31fef4ec
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-06-11 15:57:10 +0200

    feat(webapp): added comment service in frontend

---
+++ webapp/storyteller/src/app/service/comment.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {AddCommentRequest} from "../model/comment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  constructor(private http: HttpClient) { }
+
+  getCommentsByChapterId(chapterId: string) {
+    return this.http.get<Comment[]>(`${environment.apiUrl}/comments/${chapterId}`);
+  }
+
+  addComment(comment: AddCommentRequest) {
+    return this.http.post<Comment>(`${environment.apiUrl}/comments/add`, comment);
+  }
+}
