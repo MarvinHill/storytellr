6a77dc5ace970b7b022c88aa280e0b3edfa37aff
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-06-15 11:08:12 +0200

    feat(webapp): added component

---
+++ webapp/storyteller/src/app/components/comment-modal/comment-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-modal',
+  templateUrl: './comment-modal.component.html',
+  styleUrl: './comment-modal.component.scss'
+})
+export class CommentModalComponent {
+
+}
