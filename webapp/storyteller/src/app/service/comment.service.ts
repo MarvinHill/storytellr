b5e81c83a155a09f552244c079a8065291e974b7
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-06-16 10:53:57 +0200

    feat(webapp): imported correct comment class

--- webapp/storyteller/src/app/service/comment.service.ts
+++ webapp/storyteller/src/app/service/comment.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
-import {AddCommentRequest} from "../model/comment";
+import {AddCommentRequest, Comment} from "../model/comment";
 
 @Injectable({
   providedIn: 'root'
