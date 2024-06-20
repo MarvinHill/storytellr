7a7d2eed5dddab31c60e283db90c2862b4609d65
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-06-20 18:17:22 +0200

    feat(api): added comments deactivated for backend

--- api/src/main/java/de/storyteller/api/service/comment/CommentServiceImpl.java
+++ api/src/main/java/de/storyteller/api/service/comment/CommentServiceImpl.java
@@ -1,12 +1,15 @@
 package de.storyteller.api.service.comment;
 
+import de.storyteller.api.model.Book;
 import de.storyteller.api.model.Chapter;
 import de.storyteller.api.model.Comment;
+import de.storyteller.api.repository.BookRepository;
 import de.storyteller.api.repository.ChapterRepository;
 import de.storyteller.api.v1.dto.comment.AddCommentRequest;
 import de.storyteller.api.v1.dto.comment.CommentDTO;
 import de.storyteller.api.v1.mapper.CommentMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Service;
 
 
@@ -19,12 +22,19 @@ public class CommentServiceImpl implements CommentService {
     private final ChapterRepository chapterRepository;
 
     private final CommentMapper commentMapper;
+    private final BookRepository bookRepository;
 
 
     @Override
     public CommentDTO addComment(AddCommentRequest addCommentRequest) {
         Chapter chapter = this.chapterRepository.findById(addCommentRequest.getChapterId()).orElseThrow(() -> new RuntimeException("Chapter not found"));
         Comment comment = this.commentMapper.toComment(addCommentRequest);
+        // Return if comments are deactivated
+        Book book = this.bookRepository.findById(chapter.getBookId()).orElseThrow(() -> new RuntimeException("Book not found"));
+        if (!book.isCommentsDeactivated()) {
+            throw new RuntimeException("Comments are deactivated for this book");
+        }
+
         chapter.getComments().add(comment);
         this.chapterRepository.save(chapter);
         return this.commentMapper.toDTO(comment);
@@ -33,12 +43,22 @@ public class CommentServiceImpl implements CommentService {
     @Override
     public List<CommentDTO> getCommentsByChapter(String chapterId) {
         Chapter chapter = this.chapterRepository.findById(chapterId).orElseThrow(() -> new RuntimeException("Chapter not found"));
+        // Return if comments are deactivated
+        Book book = this.bookRepository.findById(chapter.getBookId()).orElseThrow(() -> new RuntimeException("Book not found"));
+        if (!book.isCommentsDeactivated()) {
+            return List.of();
+        }
         return chapter.getComments().stream().map(this.commentMapper::toDTO).toList();
     }
 
     @Override
     public List<CommentDTO> getCommentsByBlockId(String chapterId, String blockId) {
         Chapter chapter = this.chapterRepository.findById(chapterId).orElseThrow(() -> new RuntimeException("Chapter not found"));
+        // Return if comments are deactivated
+        Book book = this.bookRepository.findById(chapter.getBookId()).orElseThrow(() -> new RuntimeException("Book not found"));
+        if (!book.isCommentsDeactivated()) {
+            return List.of();
+        }
         return chapter.getComments().stream().filter(comment -> comment.getBlockId().equals(blockId)).map(this.commentMapper::toDTO).toList();
     }
 
