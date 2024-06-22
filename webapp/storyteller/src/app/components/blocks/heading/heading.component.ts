ffb4a715598b73538bb2345d7602c3380ab59412
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-06-22 11:27:39 +0200

    feat(webapp): added documentation for blocks

--- webapp/storyteller/src/app/components/blocks/heading/heading.component.ts
+++ webapp/storyteller/src/app/components/blocks/heading/heading.component.ts
@@ -5,6 +5,9 @@ import {Component, Input} from '@angular/core';
   templateUrl: './heading.component.html',
   styleUrl: './heading.component.scss'
 })
+/**
+ * HeadingComponent is a component that displays a heading in Reading Mode
+ */
 export class HeadingComponent {
   @Input() content!: string;
 
