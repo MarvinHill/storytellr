29033cb2273880b419e4c824801d24ff6ef57e0d
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-06-24 15:13:03 +0200

    feat(api): added documentation for image services

--- api/src/main/java/de/storyteller/api/service/tools/ImageEditService.java
+++ api/src/main/java/de/storyteller/api/service/tools/ImageEditService.java
@@ -1,14 +1,31 @@
 package de.storyteller.api.service.tools;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import org.im4java.core.IM4JavaException;
-import org.springframework.web.multipart.MultipartFile;
 
+/**
+ * Service for editing images.
+ */
 public interface ImageEditService {
+  /**
+   * Scale an image to a small size.
+   * @param inFile Input file.
+   * @param outFile Output file.
+   * @throws IOException If an I/O error occurs.
+   * @throws InterruptedException If the process is interrupted.
+   * @throws IM4JavaException If an error occurs during image processing.
+   */
   void scaleToSm(String inFile, String outFile)
       throws IOException, InterruptedException, IM4JavaException;
+
+  /**
+   * Crop an image to a large size.
+   * @param inFile Input file.
+   * @param outFile Output file.
+   * @throws IOException If an I/O error occurs.
+   * @throws InterruptedException If the process is interrupted.
+   * @throws IM4JavaException If an error occurs during image processing.
+   */
   void cropToLg(String inFile, String outFile)
       throws IOException, InterruptedException, IM4JavaException;
 }
